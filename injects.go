9a20821deb58db105c5341fa3e396ea84bd00453
storezhang <storezhang@gmail.com>
2022-03-07 11:12:11 +0800

    修复标签注入找不到文件的问题

--- injects.go
+++ injects.go
@@ -3,7 +3,7 @@ package main
 func (p *plugin) injects() (undo bool, err error) {
 	for lang, inputs := range p.inputsCache {
 		for _, input := range inputs {
-			if err = p.build(lang, input); nil != err {
+			if err = p.inject(lang, input); nil != err {
 				return
 			}
 		}
