eb6824cf3fce19faa4d8083884e31498140e34c8
storezhang <storezhang@gmail.com>
2022-03-11 09:50:43 +0800

    修复插件列表组装不对的问题

--- copies.go
+++ copies.go
@@ -11,12 +11,14 @@ func (p *plugin) copies() (undo bool, err error) {
 	for _, output := range p.Outputs {
 		for _, filename := range p.Copies {
 			from := filepath.Join(p.Source, filename)
-			// 检查源文件是否存在，不存在复制下一个文件
-			if !gfx.Exists(from) {
+			to := filepath.Join(output, filename)
+			// 有两种情况不应该执行文件复制
+			// 源文件不存在
+			// 目的文件已经存在
+			if !gfx.Exists(from) || gfx.Exists(to) {
 				continue
 			}
 
-			to := filepath.Join(output, filename)
 			if err = gfx.Copy(from, to); nil != err {
 				p.Error(`复制文件出错`, field.String(`from`, from), field.String(`to`, to), field.Error(err))
 			}
