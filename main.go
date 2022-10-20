5ec63beb9f1be8c87462ba359b7f9af2971c0c7b
storezhang <storezhang@gmail.com>
2022-10-20 16:23:01 +0800

    fix(Protoc): 第三方库不生效问题

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/dronestock/drone"
 )
 
 func main() {
-	for _, env := range os.Environ() {
-		fmt.Println(env)
-	}
 	panic(drone.Bootstrap(newPlugin))
 }
