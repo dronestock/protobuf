5627f471dff6252bc20ab1ee74dbbec7014f55cb
storezhang <storezhang@gmail.com>
2022-12-28 10:13:29 +0800

    fix(Protobuf): 源文件未全部写入

--- protoc.go
+++ protoc.go
@@ -21,15 +21,7 @@ func (p *plugin) protoc(input string, filenames []string, args []any) (err error
 
 	// 将需要编译的文件加入到最终的参数中
 	for _, filename := range filenames {
-		if abs, ae := filepath.Abs(filename); nil != ae {
-			err = ae
-		} else {
-			args = append(args, abs)
-		}
-
-		if nil != err {
-			return
-		}
+		args = append(args, filename)
 	}
 
 	if err = p.Exec(protocExe, drone.Args(args...), drone.Dir(filepath.Dir(input))); nil != err {
